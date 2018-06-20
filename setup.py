3a0ca9f225c74fe78375500299e8fff4e556105d
YangJinJe <yseiren87@gmail.com>
2018-06-20 13:29:31 +0900

    setup.py(version 0.1.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yangsrunner',
-    version='0.0.1',
+    version='0.1.0',
     description='Terminal Application Runner',
     long_description='Terminal Application Runner',
     keywords=['terminal'],
